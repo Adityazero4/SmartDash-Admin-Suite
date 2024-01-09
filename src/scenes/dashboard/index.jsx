36c31ffd164351e27180122bb471d48fa951287e
Adityazero4 <jainad0417@gmail.com>
2024-01-10 02:06:59 +0530

    first commit

---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Header";
+import { Box } from "@mui/material";
+
+const Dashboard = () => {
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="Dashboard" subtitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
